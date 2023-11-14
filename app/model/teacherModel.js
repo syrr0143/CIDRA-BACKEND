dae30de248a600be0bd708d1c4b81c303958a88f
sumit yadav <120999503+syrr0143@users.noreply.github.com>
2023-11-14 18:22:37 +0530

    commented out teacherModel.js

--- app/model/teacherModel.js
+++ app/model/teacherModel.js
@@ -1,27 +1,27 @@
-// models/User.js
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const validator = require('validator');
+// // models/User.js
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+// const validator = require('validator');
 
-const teacherSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  password: { type: String, required: true },
-  id: { type: String, required: true },
-  dob: { type: String, required: true },
-  email: {
-    type: String,
-    required: true,
-    validate: {
-      validator: validator.isEmail,
-      message: '{VALUE} is not a valid email',
-    },
-  },
-  mobileNumber: { type: String, required: true },
-  address: { type: String, required: true },
-  department: { type: String, required: true },
+// const teacherSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   password: { type: String, required: true },
+//   id: { type: String, required: true },
+//   dob: { type: String, required: true },
+//   email: {
+//     type: String,
+//     required: true,
+//     validate: {
+//       validator: validator.isEmail,
+//       message: '{VALUE} is not a valid email',
+//     },
+//   },
+//   mobileNumber: { type: String, required: true },
+//   address: { type: String, required: true },
+//   department: { type: String, required: true },
   
-});
+// });
 
-const teacher = mongoose.model('teacher', teacherSchema);
+// const teacher = mongoose.model('teacher', teacherSchema);
 
-module.exports = teacher;
+// module.exports = teacher;
