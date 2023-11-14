2f9105d57620e899bad8581434591481b7510234
syrr0143 <sy816120@gmail.com>
2023-11-14 18:55:19 +0530

    updated

--- app/model/complaintModel.js
+++ app/model/complaintModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const complaintSchema = new mongoose.Schema({
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  authorname: { type: mongoose.Schema.Types.String, ref: 'User', required: true },
   time: { type: Date, default: Date.now },
   content: { type: String, required: true },
 });
