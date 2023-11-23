02fd1eed158031880e96779e107c0a96cc699ce0
syrr0143 <sy816120@gmail.com>
2023-11-24 01:03:59 +0530

    updated

--- app/routes/assignmentRoute.js
+++ app/routes/assignmentRoute.js
@@ -36,6 +36,34 @@ router.post('/post-assignment', teacherAuthMiddleware, upload.single('pdf'), asy
     res.status(500).json({ error: 'Internal server error',details: error.message });
   }
 });
+// to show the assignment posted by the user
+
+router.get('/get-assignment', teacherAuthMiddleware, async (req, res) => {
+  try {
+    // The teacherauthMiddleware should set the user details in the request
+    const authorId = req.teacher._id;
+
+    // Fetch lectures posted by the logged-in teacher
+    const assignment = await Assignment.find({ author: authorId }).populate('author', 'name');
+
+    // If no lectures are found, return a message
+    if (assignment.length === 0) {
+      return res.status(404).json({ message: 'No assignment found for this teacher' });
+    }
+
+    // Format and send the response
+    const formattedassignment = assignment.map(assignment  => ({
+      content: assignment.content,
+      date: assignment.date,
+      author:assignment.author
+    }));
+
+    res.status(200).json({ assignment: formattedassignment  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 // Get all assignments
 router.get('/assignments', async (req, res) => {
